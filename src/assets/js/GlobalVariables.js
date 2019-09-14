4d86fcd1fd20c62821858ed689b0e0854c8551e9
Dimas <dimas.karpusss@gmail.com>
2019-09-14 15:16:44 +0300

    GlobalVariables.js add new variables

--- src/assets/js/GlobalVariables.js
+++ src/assets/js/GlobalVariables.js
@@ -1,1 +1,18 @@
-const g = {Input:{}, Output:{}, Storage:{}};
+const g = {
+  Input:{},
+  Output:{},
+  Storage:{},
+  Controller:{
+    CustomerProfile: {
+      customerNavTableScrollCounter: 0,
+      customerNavTableCountToSelect: 40,
+      orderNavTableScrollCounter: 0,
+      orderNavTableCountToSelect: 40
+    },
+    Cycles:{
+      cyclesNavBarScrollCounter: 0,
+      cyclesNavBarCountToSelect: 40,
+      toScroll: false
+    }
+  }
+};
