d6c96d3f7197f1339fe754edc528e91cbd295c72
Dimas <dimas.karpusss@gmail.com>
2019-11-18 23:24:30 +0100

    DAO Update

--- src/assets/js/DAO/CustomerProfileDAO.js
+++ src/assets/js/DAO/CustomerProfileDAO.js
@@ -43,136 +43,226 @@
     @do take data from needed field or fields and paste new element to html-array
 */
 class CustomerProfileDAO extends DAO{
-  constructor(){}
+  constructor(){
+    super();
+  }
 
-  addCustomersToNavTable(where, data){
-    var out = new CustomerProfileOutput();
+  addCustomersToNavTable(where, data, cb){
     var db = new CustomerTableSQL();
-    var columns = ["fullName", "telephone"];
-    var customers = table.select(where, data);
+    var out = new CustomerProfileOutput();
+    var columns = ["fullName", "telephones"];
     var tableObject = {body:[]};
 
-    db.select(where, data).forEach(customer => {
-      let row = [];
-      row[row.length] = {id: customer.id};
-      for(var i = 0; i < columns.length; i++) row[row.length] = [customer[columns[i]], 1, 1];
-      tableObject.body[tableObject.body.length] = row;
+    db.select(where, data, customers => {
+      console.log(customers);
+      customers.forEach(customer => {
+        let row = [];
+        row[row.length] = {id: customer.id};
+        for(var i = 0; i < columns.length; i++){
+          let val = customer[columns[i]];
+
+          if(typeof val == "object") val = JSON.stringify(val).slice(4, -1).split("\",\"").join("<br>").replace(/\"/g, "");
+
+          row[row.length] = [val, 1, 1];
+        }
+        tableObject.body[tableObject.body.length] = row;
+      });
+      if(customers[0]) g.Controller.CustomerProfile.customerNavTableScrollCounter = -1;
+      out.insertData("WP_Tbl", tableObject);
+      if(cb) cb();
     });
-    if(customers[0]) g.Controller.CustomerProfile.customerNavTableScrollCounter = -1;
-    out.insertData("WP_Tbl", tableObject);
   }
 
-  fillCustomerWin(customerID){
+  fillCustomerWin(customerID, cb){
     var out = new CustomerProfileOutput();
     var db = new CustomerTableSQL();
-    var customer = db.load([customerID])[0] | {fullName:"", telephones: [], adresses: [], email: "", notes: "", preferences: "", socialMedias: [], activity: "", isNotEmpty: false};
-    var columns = ["fullName", "telephone"];
-    var tableObject = {body:[]};
+    db.load([customerID], customers => {
+      var customer = customers[0];
+      customer = customer!=undefined?customer:{fullName:"", telephones: [""], adresses: [""], email: "", notes: "", preferences: "", socialMedia: [""], activity: "", isEmpty: true};
+      var columns = ["fullName", "telephone"];
 
-    out.insertData("WP_ID", customerID);
-    out.insertData("WP_Nm", customer["fullName"]);
-    out.insertData("WP_Tel", ["", customer["telephones"]]);
-    out.insertData("WP_Adr", ["", customer["adresses"]]);
-    out.insertData("WP_E-m", customer["email"]);
-    out.insertData("WP_Nt", customer["notes"]);
-    out.insertData("WP_Pr", customer["preferences"]);
-    out.insertData("WP_SM", ["", customer["socialMedias"]]);
-    out.insertData("WP_Act", customer["activity"]);
+      out.insertData("WP_ID", customerID);
+      out.insertData("WP_Nm", customer["fullName"]);
+      out.insertData("WP_Tel", customer["telephones"]);
+      out.insertData("WP_Adr", customer["adresses"]);
+      out.insertData("WP_E-m", customer["email"]);
+      out.insertData("WP_Nt", customer["notes"]);
+      out.insertData("WP_Pr", customer["preferences"]);
+      out.insertData("WP_SM", customer["socialMedia"]);
+      out.insertData("WP_Act", customer["activity"]);
 
-    if(!customer.isEmpty) this.addOrdersToTable(customerID);
+      g.Controller.CustomerProfile.orderNavTableScrollCounter = 0;
+
+      out.insertData("WO_Tbl", {body: -1});
+
+      if(!customer.isEmpty) this.addOrdersToTable(customerID, undefined, cb);
+    });
   }
 
-  addOrdersToTable(customerID, orderID){
+  addOrdersToTable(customerID, orderID, cb, isNew){
+    customerID = parseInt(customerID);
     let s = (new CustomerProfileInput()).takeData("WO_SF");
     s = (s != "")?s:undefined;
-    let q = ""; //create query for getted %s
-    db = new OrderTableSQL();
-    let orders = g.Controller.CustomerProfile.orderNavTableScrollCounter =! -1
-                      ?db.select(
-                        (orderID?'id = ?':"customerID = ?") + q +" ORDER BY id DESC LIMIT ?, ?",
-                        [
-                          orderID | customerID,
+    if(s) g.Controller.CustomerProfile.orderNavTableScrollCounter = 0;
+    let db = new OrderTableSQL();
+    let dbCycle = new CycleTableSQL();
+    let out = new CustomerProfileOutput();
+
+    if(g.Controller.CustomerProfile.orderNavTableScrollCounter != -1) dbCycle.select("1=1", [], cycles => {
+                      db.select(
+                        (orderID!=undefined?'id = ?':"customerID = ?") + (isNew?"":" ORDER BY id DESC LIMIT ?, ?"),
+                        isNew?[orderID]:[
+                          orderID!=undefined?orderID:customerID,
                           g.Controller.CustomerProfile.orderNavTableScrollCounter*g.Controller.CustomerProfile.orderNavTableCountToSelect,
                           g.Controller.CustomerProfile.orderNavTableCountToSelect
-                        ]
-                      ):[];
-    if(orders[0]){
-      orders.forEach(order => {
-          let row = [];
-          row[row.length] = {id: order.id};
-          for(var i = 0; i < columns.length; i++) row[row.length] = [order[columns[i]], 1, 1];
-          tableObject.body[tableObject.body.length] = row;
-        });
-      out.insertData("WO_Tbl", tableObject);
-      g.Controller.CustomerProfile.orderNavTableScrollCounter++;
-    }else g.Controller.CustomerProfile.orderNavTableScrollCounter = -1;
+                        ],
+                        orders => {
+                          console.log(orders);
+                          if(orders.length > 0){
+                            let columns;
+                            var tableObject = {body:[]};
+                            columns = ["cycleID", "kits", "products", "adress", "summary", "billed", "pays", "payDates", "orderNotes", "socialMedia"];
+                            orders.forEach(order => {
+                                let row = [];
+                                row[row.length] = {id: order.id};
+                                for(var i = 0; i < columns.length; i++) row[row.length] = [convertField(order, columns[i]), 1, 1];
+                                tableObject.body[tableObject.body.length] = row;
+                              });
+                              console.log(tableObject);
+                            out.insertData("WO_Tbl", tableObject);
+                            g.Controller.CustomerProfile.orderNavTableScrollCounter++;
+                          }else g.Controller.CustomerProfile.orderNavTableScrollCounter = -1;
+
+                          if(cb) cb();
+
+                          function convertField(o, c){
+                            switch(c){
+                              case "cycleID":
+                                let r = cycles.find(cycle => cycle.id == o[c])
+                                return r?r.name:"Not Selected";
+                                break;
+                              case "kits":
+                                return Object.keys(o.kits).join(", ");
+                                break;
+                              case "products":
+                                return Object.keys(o.kits).map(kitName => kitName + ": [" + o.kits[kitName].products.map(pr => pr.name + ": " + pr.count).join(", ") + "]").join("<br>");
+                                break;
+                              case "pays":
+                                o[c].shift();
+                                return o[c].join(", ");
+                              case "payDates":
+                                o[c].shift();
+                                return o[c].join(", ");
+                              default:
+                                return o[c];
+                                break;
+                            }
+                          }
+                        }
+                      )
+                    });
   }
 
-  fillOrderCreatingWin(customerID, orderID){
+  fillOrderCreatingWin(customerID, orderID, cb){
     var dbOrder = new OrderTableSQL();
     var dbCustomer = new CustomerTableSQL();
     var dbCycle = new CycleTableSQL();
     var dbKit = new KitTableSQL();
-    var out = new CycleOutput();
-    var order = dbOrder.load([orderID])[0] | {id: -1, telephone: "", socialMedia: "", adress: "", orderNotes: "", summary: 0, billed: false, isNotThis: false, payDates: [], pays: [], cycleID: -1, customerID: customerID};
-    var customer = dbCustomer.load([order["customerID"]])[0] | {fullName:"", telephones: [], adresses: [], email: "", notes: "", preferences: "", socialMedias: [], activity: "", isNotEmpty: false};
+    var out = new CustomerProfileOutput();
+    var ordersF = customer => {
+      dbOrder.load([orderID], orders => {
+        var order = orders[0];
+        order = order?order:{id: -1, telephone: [""], socialMedia: [""], adress: [""], orderNotes: "", summary: 0, billed: false, isNotThis: false, payDates: [""], pays: [""], cycleID: -1, customerID: customerID};
 
-    //fill fields from customer object
-    out.insertData("AW_Nm", customer["fullName"]);
-    out.insertData("AW_Pr", customer["Preferences"]);
-    //fill fields from customer and order objects
-    out.insertData("AW_Tel", [order["telephone"], ...customer["telephones"]].filter(tel => tel != order["telephone"]));
-    out.insertData("AW_SM", [order["socialMedia"], ...customer["socialMedias"]].filter(media => media != order["socialMedia"]));
-    out.insertData("AW_Adr", [order["adress"], ...customer["adresses"]].filter(adress => adress != order["adress"]));
-    //fill fields from order object
-    out.insertData("AW_ID", order["id"]);
-    out.insertData("AW_ON", order["orderNotes"]);
-    out.insertData("AW_Sum", order["summary"]);
-    out.insertData("AW_Bill", order["billed"]);
-    out.insertData("AW_n_ths", order["isNotThis"]);
-    out.insertData("AW_an_FN", order["isNotThis"]?order["anotherFullName"]:"");
-    out.insertData("AW_an_Tel", order["isNotThis"]?order["anotherTelephone"]:"");
-    out.insertData("AW_P", order["payDates"].map((d, i) => [d, order["pays"][i]]));
-    out.insertData("AW_c_id",
-                  [
-                    order["cycleID"] != -1?[
-                      order["cycleID"],
-                      dbCycle.load([order["cycleID"]])[0]["name"]
-                    ]: [-1, "Select Cycle"],
-                    ...dbCycle.select("TRUE")
-                              .map(cycle => [cycle["id"], cycle["name"]])
-                              .filter(cycle => order["cycleID"] != -1 || cycle[0] != order["cycleID"])
-                  ]);
+        out.insertData("AW_Opn", true);
 
-    var kits = {...dbKit.load(dbCycle.load([cycleID])[0]["kitsID"])} | {};
-    var kitsData = {};
-    for(var k in kits){
-      kitsData[kits[k]["name"]] = Object.assign({}, kits[k]);
-    }
-    if(order["cycleID"] != -1){
-      kits = order["kits"].map(kit => kit.products.sort((p1, p2) => p2.count - p1.count));
-      var kitsData = {};
-      var kitsFromDB = Object.assign({}, dbKit.load(dbCycle.load([order["cycleID"]])[0]["kitsID"]));
-      for(var k in kits){
-        kitsData[kits[k]["name"]] = Object.assign({}, kits[k]);
-        delete kits[k];
-      }
-      for(var k in kitsFromDB) if(!kitsData[kitsFromDB[k]["name"]]) kitsData[kitsFromDB[k]["name"]] = Object.assign({}, kitsFromDB[k]);
-      out.insertData("AW_kits", kitsData);
-    }
+        //fill fields from customer object
+        out.insertData("AW_Nm", customer["fullName"]);
+        out.insertData("AW_Pr", customer["preferences"]);
+        //fill fields from customer and order objects
+        out.insertData("AW_Tel", [order["telephone"][0], ...customer["telephones"].filter(tel => tel != order["telephone"][0])]);
+        out.insertData("AW_SM", [order["socialMedia"][0], ...customer["socialMedia"].filter(media => media != order["socialMedia"][0])]);
+        out.insertData("AW_Adr", [order["adress"][0], ...customer["adresses"].filter(adress => adress != order["adress"][0])]);
+        //fill fields from order object
+        out.insertData("AW_ID", order["id"]);
+        out.insertData("AW_ON", order["orderNotes"]);
+        out.insertData("AW_Sum", order["summary"]);
+        out.insertData("AW_Bill", order["billed"] == "true");
+        out.insertData("AW_n_ths", order["isNotThis"] == "true");
+        out.insertData("AW_an_FN", order["isNotThis"] == "true"?order["anotherFullName"]:"");
+        out.insertData("AW_an_Tel", order["isNotThis"] == "true"?order["anotherTelephone"]:"");
+        out.insertData("AW_P", order["payDates"].map((d, i) => [d, order["pays"][i]]));
+        //clear kits
+        out.insertData("AW_kits", {});
+
+        if(order["cycleID"] == -1){
+          dbCycle.select("TRUE", [], cycles => {
+            out.insertData("AW_c_id", [[-1, "Select Cycle"], ...cycles.map(cycle => [cycle["id"], cycle["name"]]) ]);
+          });
+        }else{
+          dbCycle.select("TRUE", [], cycles => {
+            var selectedCycle = cycles.filter(cycle => cycle.id == order.cycleID)[0];
+            var restCycles = cycles.filter(cycle => cycle.id != order.cycleID);
+            out.insertData("AW_c_id", [ [selectedCycle.id, selectedCycle.name], ...restCycles.map(cycle => [cycle.id, cycle.name]) ]);
+
+            dbKit.load(selectedCycle["kitsID"], kitsFromDB => {
+              kitsFromDB.forEach((kit, i) => {
+                var orderKit = order.kits[kit.name];
+                if(!orderKit) return false;
+
+                kitsFromDB[i].count = (typeof orderKit.count == "number")?orderKit.count:0;
+                kitsFromDB[i].price = (typeof orderKit.price == "number")?(orderKit.price):(kitsFromDB[i].price);
+
+                var names = {};
+                orderKit.products.forEach((pr, i) => names[pr.name] = i);
+
+                if(JSON.stringify(names) != "{}"){
+                  kit.products.forEach((pr, i) => {
+                    if(names[pr.name] !== undefined){
+                      kit.products[i].count = orderKit.products[names[pr.name]].count;
+                      kit.products[i].price.selected = orderKit.products[names[pr.name]].price.selected;
+                    }
+                  });
+                }
+              });
+
+              var kitsData = {};
+
+              kitsFromDB.forEach(kit => {
+                kitsData[kit.name] = kit;
+              });
+              console.log(order.kits, kitsData, kitsFromDB);
+              out.insertData("AW_kits", kitsData);
+
+              if(cb) cb();
+            });
+          });
+        }
+      });
+    };
+
+    dbCustomer.load([customerID], customers => {
+      ordersF(customers[0]);
+    });
   }
 
   displayKits(cycleID){
     var out = new CustomerProfileOutput();
     var dbCycle = new CycleTableSQL();
     var dbKit = new KitTableSQL();
-    var kits = {...dbKit.load(dbCycle.load([cycleID])[0]["kitsID"])} | {};
-    var kitsData = {};
-    if(!kits[Obj])
-    for(var k in kits){
-      kitsData[kits[k]["name"]] = Object.assign({}, kits[k]);
-    }
-    out.insertData("AW_kits", kitsData);
+
+    dbCycle.load([cycleID], cycles => {
+      if(cycles.length > 0){
+        dbKit.load(cycles[0]["kitsID"], kits => {
+          var kitsData = {};
+          for(var k in kits){
+            if(!kits[k].count) kits[k].count = 0;
+            kitsData[kits[k]["name"]] = Object.assign({}, kits[k]);
+          }
+          out.insertData("AW_kits", kitsData);
+        });
+      }else out.insertData("AW_kits", {});
+    });
   }
 
   deleteCustomer(customerID){
@@ -202,14 +292,14 @@ class CustomerProfileDAO extends DAO{
       email: inp.takeData("WP_E-m"),
       notes: inp.takeData("WP_Nt"),
       preferences: inp.takeData("WP_Pr"),
-      socialMedias: inp.takeData("WP_SM"),
+      socialMedia: inp.takeData("WP_SM"),
       activity: inp.takeData("WP_Act")
     });
   }
 
   getOrderFromPage(){
-    var inp = new CustomerProfileInput();
-    return [new OrderModel({
+    let inp = new CustomerProfileInput();
+    let order = new OrderModel({
       id: inp.takeData("AW_ID"),
       cycleID: inp.takeData("AW_c_id"),
       telephone: inp.takeData("AW_Tel"),
@@ -219,27 +309,43 @@ class CustomerProfileDAO extends DAO{
       summary: inp.takeData("AW_Sum"),
       billed: inp.takeData("AW_Bill"),
       isNotThis: inp.takeData("AW_n_ths"),
-      payDates: inp.takeData("AW_PD"),
-      pays: inp.takeData("AW_Pay"),
       customerID: inp.takeData("WP_ID"),
       anotherFullName: inp.takeData("AW_an_FN"),
       anotherTelephone: inp.takeData("AW_an_Tel"),
       kits: inp.takeData("AW_kits")
-    }), {...this.getCustomerFromPage(), preferences: inp.takeData("AW_Pr")}];
+    });
+    let customer = this.getCustomerFromPage();
+    customer.preferences = inp.takeData("AW_Pr");
+    if(!customer.telephones.find(t => t==order.telephone)) customer.telephones.push(order.telephone[0]);
+    if(!customer.adresses.find(o => o==order.adress)) customer.adresses.push(order.adress[0]);
+    if(!customer.socialMedia.find(o => o==order.socialMedia)) customer.socialMedia.push(order.socialMedia[0]);
+
+    const payData = inp.takeData("AW_P");
+    let pays = payData.map(data => data[0]);
+    let dates = payData.map(data => data[1]);
+
+    order.pays = pays;
+    order.payDates = dates;
+
+    return [order, customer];
   }
 
-  saveCustomer(customer){
-    let db = CustomerTableSQL();
+  saveCustomer(customer, cb){
+    let db = new CustomerTableSQL();
     let id = customer["id"];
-    customer = db.save([customer])[0];
-    return id != customer["id"]?customer:undefined;
+    if(id==-1) delete customer["id"];
+    db.save([customer], customers => {
+      cb(customers[0]);
+    });
   }
 
-  saveOrder(order){
-    let db = OrderTableSQL();
+  saveOrder(order, cb){
+    let db = new OrderTableSQL();
     let id = order["id"];
-    order = db.save([order])[0];
-    return order;
+    if(id==-1) delete order["id"];
+    db.save([order], orders => {
+      cb(orders[0]);
+    });
   }
 
   addToArray(inpName, emp){
@@ -247,7 +353,24 @@ class CustomerProfileDAO extends DAO{
     let inp = new CustomerProfileInput();
     let out = new CustomerProfileOutput();
     let vals = inp.takeData(inpName);
-    out.insertData(inpName, [emp, ...vals]);
+
+    vals[vals.length] = JSON.parse(JSON.stringify(vals[0]));
+    vals[0] = emp;
+
+    out.insertData(inpName, vals);
+  }
+
+  selectTable(search){
+    Array.from((new CustomerProfileInput()).takeData("Tbl_rows")).forEach(tr => {
+      var hasSearched = false;
+
+      Array.from(tr.querySelectorAll("td")).forEach(td => {
+        if(td.innerHTML.search(search) > -1) hasSearched = true;
+      });
+
+      if(hasSearched) tr.style.display = "table-row";
+      else tr.style.display = "none";
+    });
   }
 
 }
