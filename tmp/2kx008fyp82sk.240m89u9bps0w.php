e2455723661dfd369628c782acba576f5ee64976
Dimas <dimas.karpusss@gmail.com>
2019-12-20 20:20:18 +0100

    Cleaned unuseful files

--- tmp/2kx008fyp82sk.240m89u9bps0w.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="/crm/functions/startSession">
-<input placeholder="username" type="text" name="username"/>
-<input placeholder="password" type="text" name="pass"/>
-<button>Submit</button>
-</form>
